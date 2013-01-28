254c8988c67fee02cd03282451081f0d9e8f769b
christopher and mario <christopherlives@gmail.com>
2013-01-28 10:43:38 -0800

    Initial commit

---
+++ sakila_demo/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SakilaDemo::Application.config.secret_token = '9a0fe4c065910cb263752ac5456365b00d1a785b1a6e901b1f84e6558ae30991db440fce16ce77db42109c0655c39c70e26d77ba9a067cd7f68320a73e42952a'
